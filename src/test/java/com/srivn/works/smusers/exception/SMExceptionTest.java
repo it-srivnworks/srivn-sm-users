1b9a6641bc8854981d3a60f3ff6dab7678f976b5
dev02-srivnworks <dev02.srivnworks@gmail.com>
2023-04-30 18:51:12 +0100

    Added Junit Test case

---
+++ src/test/java/com/srivn/works/smusers/exception/SMExceptionTest.java
@@ -0,0 +1,18 @@
+package com.srivn.works.smusers.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.srivn.works.smusers.util.AppMsg;
+import org.junit.jupiter.api.Test;
+
+class SMExceptionTest {
+    @Test
+    void test_getSMException() {
+        // Arrange and Act
+        SMException actualSMException = SMException.getSMException(AppMsg.Err.ERR_UKN_000, "Xtra String");
+        // Assert
+        assertEquals("Unknown Error, Please Try Again!", actualSMException.getMessage());
+        assertEquals("UNKNOWN", actualSMException.getExType());
+    }
+}
+
